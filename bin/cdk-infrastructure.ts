42858116fcdaf08915eb35ac68f662790fa25dca
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-22 13:28:16 +1000

    Ready made CDK infrastructure for cdk-app and cdk-app2

--- bin/cdk-infrastructure.ts
+++ bin/cdk-infrastructure.ts
@@ -3,15 +3,21 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkInfrastructureStack } from '../lib/cdk-infrastructure-stack';
 
+// const account = new cdk.CfnParameter(cdk.Construct, "accountNumber", {
+//   type: "String",
+//   description: "Your AWS Account Number"
+// })
+
 const app = new cdk.App();
-new CdkInfrastructureStack(app, 'CdkInfrastructureStack', {
+const stackName = app.node.tryGetContext('stackName');
+new CdkInfrastructureStack(app, stackName, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- lib/cdk-infrastructure-stack.ts
+++ lib/cdk-infrastructure-stack.ts
@@ -1,9 +1,76 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { Vpc } from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecr from "@aws-cdk/aws-ecr";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import { Compatibility } from "@aws-cdk/aws-ecs";
 
 export class CdkInfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // CLI context input
+    const clusterName = this.node.tryGetContext('clusterName');
+    const serviceName = this.node.tryGetContext('serviceName');
+    const assetFolder = this.node.tryGetContext('assetFolder');
+
+    console.log(clusterName);
+    console.log(serviceName);
+    console.log(assetFolder);
+
+    // VPC
+    const vpc = new Vpc(this, "infrastructureVpcId", {
+      maxAzs: 2,
+      natGateways: 1
+  });
+
+    // Cluster
+    const cluster = new ecs.Cluster(this, "infrastructureClusterId", {
+        clusterName: clusterName,
+        vpc: vpc as any,
+    });
+
+    // const taskDef = new ecs.TaskDefinition(this, assetFolder+"TaskDef", {
+    //   compatibility: Compatibility.FARGATE,
+    //   memoryMiB: "1024",
+    //   cpu: "512"
+    // });
+
+    // taskDef.addContainer(assetFolder+"Container", {
+    //   image: ecs.ContainerImage.fromAsset(assetFolder)
+    // })
+
+    // // Fargate service
+    // const service = new ecs.FargateService(this, "infrastructureServiceId", {
+    //     cluster: cluster,
+    //     serviceName: serviceName,
+    //     assignPublicIp: true,
+    //     desiredCount: 1,
+    //     taskDefinition: taskDef
+    // });
+
+    // // Health check
+    // //service.targetGroup.configureHealthCheck({path: "/health"});
+
+    const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "infrastructureServiceId", {
+      cluster: cluster,
+      serviceName: serviceName,
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      desiredCount: 1,
+      taskImageOptions: {
+          image: ecs.ContainerImage.fromAsset(assetFolder),
+      }
+    });
+
+    // Health check
+    service.targetGroup.configureHealthCheck({path: "/health"});
+
+    // Load balancer url
+    new cdk.CfnOutput(this, "loadBalancedUrlId", {
+        value: service.loadBalancer.loadBalancerDnsName,
+        exportName: "loadBalancerUrl",
+    });
+
   }
 }
