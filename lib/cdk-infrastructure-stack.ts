c6edaeb4ab7996968a09312e07e722ea507aacc7
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-23 01:26:41 +1000

    Added extra customisability to the CDK repo

--- lib/cdk-infrastructure-stack.ts
+++ lib/cdk-infrastructure-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from "@aws-cdk/core";
-import { Vpc } from "@aws-cdk/aws-ec2";
+import { Vpc, SecurityGroup, Port, Peer } from "@aws-cdk/aws-ec2";
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as ecr from "@aws-cdk/aws-ecr";
 import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
@@ -14,40 +14,47 @@ export class CdkInfrastructureStack extends cdk.Stack {
     const serviceName = this.node.tryGetContext('serviceName');
     const assetFolder = this.node.tryGetContext('assetFolder');
 
-    console.log(clusterName);
-    console.log(serviceName);
-    console.log(assetFolder);
+    const port: number = this.node.tryGetContext('port') || 80;
+    const memoryLimitMiB: number = this.node.tryGetContext('memoryLimitMiB') || 1024;
+    const cpu: number = this.node.tryGetContext('cpu') || 256;
 
     // VPC
     const vpc = new Vpc(this, "infrastructureVpcId", {
       maxAzs: 2,
       natGateways: 1
-  });
-
-    // Cluster
-    const cluster = new ecs.Cluster(this, "infrastructureClusterId", {
-        clusterName: clusterName,
-        vpc: vpc as any,
     });
 
-    // const taskDef = new ecs.TaskDefinition(this, assetFolder+"TaskDef", {
-    //   compatibility: Compatibility.FARGATE,
-    //   memoryMiB: "1024",
-    //   cpu: "256"
-    // });
+      // Cluster
+      const cluster = new ecs.Cluster(this, "infrastructureClusterId", {
+          clusterName: clusterName,
+          vpc: vpc as any,
+      });
 
-    // taskDef.addContainer(assetFolder+"Container", {
-    //   image: ecs.ContainerImage.fromAsset(assetFolder)
-    // })
+      const taskDef = new ecs.FargateTaskDefinition(this, assetFolder+"TaskDef", {
+        memoryLimitMiB: memoryLimitMiB,
+        cpu: cpu
+      });
 
-    // // Fargate service
-    // const service = new ecs.FargateService(this, "infrastructureServiceId", {
-    //     cluster: cluster,
-    //     serviceName: serviceName,
-    //     assignPublicIp: true,
-    //     desiredCount: 1,
-    //     taskDefinition: taskDef
-    // });
+      taskDef.addContainer(assetFolder+"Container", {
+        image: ecs.ContainerImage.fromAsset(assetFolder)
+      });
+
+      const mySecurityGroup = new SecurityGroup(this, 'SecurityGroup', {
+        vpc,
+        description: 'Allow port connection to ec2 instances',
+        allowAllOutbound: true,   // Can be set to false
+      });
+      mySecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(port), `Allows the Internet to send requests to the app via port ${port}`);
+
+      // Fargate service
+      const service = new ecs.FargateService(this, "infrastructureServiceId", {
+          cluster: cluster,
+          serviceName: serviceName,
+          assignPublicIp: true,
+          desiredCount: 1,
+          taskDefinition: taskDef,
+          securityGroups: [mySecurityGroup]
+      });
 
     // //const featchedService = ecs.FargateService.fromFargateServiceArn(this, "fetchedFargateService", "arn:aws:ecs:us-east-2:571374152725:service/psgen-cluster/psgen-service-20-08-21");
     
@@ -57,25 +64,25 @@ export class CdkInfrastructureStack extends cdk.Stack {
 
 
 
-    const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "infrastructureServiceId", {
-      cluster: cluster,
-      serviceName: serviceName,
-      memoryLimitMiB: 1024,
-      cpu: 256,
-      desiredCount: 1,
-      taskImageOptions: {
-          image: ecs.ContainerImage.fromAsset(assetFolder),
-      }
-    });
+    // const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "infrastructureServiceId", {
+    //   cluster: cluster,
+    //   serviceName: serviceName,
+    //   memoryLimitMiB: 1024,
+    //   cpu: 256,
+    //   desiredCount: 1,
+    //   taskImageOptions: {
+    //       image: ecs.ContainerImage.fromAsset(assetFolder),
+    //   }
+    // });
 
-    // Health check
-    service.targetGroup.configureHealthCheck({path: "/health"});
+    // // Health check
+    // service.targetGroup.configureHealthCheck({path: "/health"});
 
-    // Load balancer url
-    new cdk.CfnOutput(this, "loadBalancedUrlId", {
-        value: service.loadBalancer.loadBalancerDnsName,
-        exportName: "loadBalancerUrl",
-    });
+    // // Load balancer url
+    // new cdk.CfnOutput(this, "loadBalancedUrlId", {
+    //     value: service.loadBalancer.loadBalancerDnsName,
+    //     exportName: "loadBalancerUrl",
+    // });
 
   }
 }
