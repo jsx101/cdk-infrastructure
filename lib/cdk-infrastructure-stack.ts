7a3a6fcdbc78d6e598cc686d9f083f675ebc1091
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-23 10:00:23 +1000

    parses context input from string to int for port, cpu, and memory values. no checker in place yet

--- lib/cdk-infrastructure-stack.ts
+++ lib/cdk-infrastructure-stack.ts
@@ -10,13 +10,13 @@ export class CdkInfrastructureStack extends cdk.Stack {
     super(scope, id, props);
 
     // CLI context input
-    const clusterName = this.node.tryGetContext('clusterName');
-    const serviceName = this.node.tryGetContext('serviceName');
-    const assetFolder = this.node.tryGetContext('assetFolder');
+    const clusterName: string = this.node.tryGetContext('clusterName');
+    const serviceName: string = this.node.tryGetContext('serviceName');
+    const assetFolder: string = this.node.tryGetContext('assetFolder');
 
-    const port: number = this.node.tryGetContext('port') || 80;
-    const memoryLimitMiB: number = this.node.tryGetContext('memoryLimitMiB') || 1024;
-    const cpu: number = this.node.tryGetContext('cpu') || 256;
+    const port: number = parseInt(this.node.tryGetContext('port')) || 80;
+    const memoryLimitMiB: number = parseInt(this.node.tryGetContext('memoryLimitMiB')) || 1024;
+    const cpu: number = parseInt(this.node.tryGetContext('cpu')) || 256;
 
     // VPC
     const vpc = new Vpc(this, "infrastructureVpcId", {
@@ -24,37 +24,37 @@ export class CdkInfrastructureStack extends cdk.Stack {
       natGateways: 1
     });
 
-      // Cluster
-      const cluster = new ecs.Cluster(this, "infrastructureClusterId", {
-          clusterName: clusterName,
-          vpc: vpc as any,
-      });
+    // Cluster
+    const cluster = new ecs.Cluster(this, "infrastructureClusterId", {
+        clusterName: clusterName,
+        vpc: vpc as any,
+    });
 
-      const taskDef = new ecs.FargateTaskDefinition(this, assetFolder+"TaskDef", {
-        memoryLimitMiB: memoryLimitMiB,
-        cpu: cpu
-      });
+    const taskDef = new ecs.FargateTaskDefinition(this, assetFolder+"TaskDef", {
+      memoryLimitMiB: memoryLimitMiB,
+      cpu: cpu
+    });
 
-      taskDef.addContainer(assetFolder+"Container", {
-        image: ecs.ContainerImage.fromAsset(assetFolder)
-      });
+    taskDef.addContainer(assetFolder+"Container", {
+      image: ecs.ContainerImage.fromAsset(assetFolder)
+    });
 
-      const mySecurityGroup = new SecurityGroup(this, 'SecurityGroup', {
-        vpc,
-        description: 'Allow port connection to ec2 instances',
-        allowAllOutbound: true,   // Can be set to false
-      });
-      mySecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(port), `Allows the Internet to send requests to the app via port ${port}`);
+    const mySecurityGroup = new SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'Allow port connection to ec2 instances',
+      allowAllOutbound: true,   // Can be set to false
+    });
+    mySecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(port), `Allows the Internet to send requests to the app via port ${port}`);
 
-      // Fargate service
-      const service = new ecs.FargateService(this, "infrastructureServiceId", {
-          cluster: cluster,
-          serviceName: serviceName,
-          assignPublicIp: true,
-          desiredCount: 1,
-          taskDefinition: taskDef,
-          securityGroups: [mySecurityGroup]
-      });
+    // Fargate service
+    const service = new ecs.FargateService(this, "infrastructureServiceId", {
+        cluster: cluster,
+        serviceName: serviceName,
+        assignPublicIp: true,
+        desiredCount: 1,
+        taskDefinition: taskDef,
+        securityGroups: [mySecurityGroup]
+    });
 
     // //const featchedService = ecs.FargateService.fromFargateServiceArn(this, "fetchedFargateService", "arn:aws:ecs:us-east-2:571374152725:service/psgen-cluster/psgen-service-20-08-21");
     
